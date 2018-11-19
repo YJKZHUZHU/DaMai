d5c78a07dd0e417539ce11888a13c98e42cf06b5
wawawa <2730455209@qq.com>
2018-11-19 10:39:01 +0800

    damai init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Button } from 'antd-mobile';
+import './style/base.scss'
 class App extends Component {
   render() {
     return (
       <div>
-        <Button>default</Button>
-        <Button type="primary">primary</Button>
+       
       </div>
     );
   }
